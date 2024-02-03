110a9a36680a340a4bdfb15277af3c3c3385006d
benhduong <77896857+benhduong@users.noreply.github.com>
2024-02-03 14:45:01 -0500

    feat: framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,22 @@
-import "./App.css";
+import { Input, InputGroup, InputLeftAddon, Text } from "@chakra-ui/react";
+import { SearchIcon } from "@chakra-ui/icons";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Search for classes below!</p>
-        <form>
-          <input type="text"></input>
-        </form>
-      </header>
-    </div>
+    <>
+      <Text>Search for classes below!</Text>
+      <InputGroup>
+        <InputLeftAddon pointerEvents="none">
+          <SearchIcon color="gray.300" />
+        </InputLeftAddon>
+        <Input
+          width="70%"
+          background="white"
+          justifyContent="center"
+          placeholder="Search here"
+        />
+      </InputGroup>
+    </>
   );
 }
 
