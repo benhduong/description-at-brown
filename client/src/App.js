6bd590e909bd6f8d25970f67c86c229aaf879957
benhduong <77896857+benhduong@users.noreply.github.com>
2024-02-03 12:50:06 -0500

    feat: create react app

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Search for classes below!</p>
+        <form>
+          <input type="text"></input>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
