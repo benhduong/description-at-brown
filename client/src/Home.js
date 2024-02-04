bcd86e23a79973c4cb6435e77a564bb23798cbd5
indiameicorengold <77947611+indiameicorengold@users.noreply.github.com>
2024-02-03 19:19:57 -0500

    dummy data

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,29 +1,68 @@
 import {
   Input,
   InputGroup,
-  InputLeftAddon,
+  InputRightAddon,
   Text,
   Flex,
   Box,
   Center,
+  Button,
+  SlideFade,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
+import React, { useState } from "react";
+import Search from "./Search";
 
 function Home() {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [showResults, setShowResults] = useState(false);
+
+  const handleChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (searchQuery === "") {
+      setShowResults(false);
+    } else {
+      setShowResults(true);
+    }
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSubmit(event);
+    }
+  };
+
   return (
-    <Box mt="20%" textAlign={"center"}>
-      <Text>Search for classes below!</Text>
-      <InputGroup>
-        <InputLeftAddon pointerEvents="none" marginLeft="auto">
-          <SearchIcon color="gray.300" />
-        </InputLeftAddon>
+    <Box mt="10%" textAlign="center">
+      <InputGroup onSubmit={handleSubmit}>
         <Input
-          marginRight="auto"
-          width="70%"
-          background="white"
-          placeholder="Search here"
+          type="text"
+          id="search"
+          value={searchQuery}
+          onChange={handleChange}
+          placeholder="Enter your search query"
+          width="60%"
+          marginLeft="auto"
+          onKeyDown={handleKeyPress}
+          isRequired
         />
+        <InputRightAddon
+          cursor="pointer"
+          marginRight="auto"
+          onClick={handleSubmit}
+        >
+          <SearchIcon color="gray.300" />
+        </InputRightAddon>
       </InputGroup>
+      {showResults && (
+        <SlideFade in={showResults} reverse>
+          <Search />
+        </SlideFade>
+      )}
     </Box>
   );
 }
--- client/src/Search.js
+++ client/src/Search.js
@@ -6,16 +6,19 @@ import {
   Flex,
   Box,
   Center,
+  SlideFade,
+  Card,
 } from "@chakra-ui/react";
 import { SearchIcon } from "@chakra-ui/icons";
 
-function About() {
+const data = [1, 2, 3, 4, 5, 6];
+
+function Search() {
   return (
     <Box mt="20%" textAlign={"center"}>
-      <Text>About</Text>
-      <Text>Created by Avi, Ben, India, and Kazen at H@B2024</Text>
+      {data && data.map((course, key) => <Card key={key}>{course}</Card>)}
     </Box>
   );
 }
 
-export default About;
+export default Search;
